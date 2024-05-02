ab1f640f85ec3bd3a0f774c9bd75a8495eadd4d2
NinoPapava <gv.papava@gmail.com>
2024-05-02 23:59:28 +0400

    (CRA) create-react-app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { RegistrationForm } from "./components/Registration/RegistrationForm";
+
+function App() {
+  return (
+    <div>
+      <RegistrationForm />
+    </div>
+  );
+}
+
+export default App;
